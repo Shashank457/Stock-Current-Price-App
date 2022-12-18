3ac3e7331ea563000027a97786506c033cc1b9e4
Shashank457 <mishrashashank457@gmail.com>
2022-12-18 14:35:43 +0530

    second

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,13 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(require('./routes/auth'))
 
+if(process.env.NODE_ENV=='production'){
+  const path=require('path')
+  app.get('/',(req,res)=>{
+    app.use(express.static(path.resolve(__dirname,'Frontend','build')))
+    res.sendFile(path.resolve(__dirname,'Frontend','build','index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
